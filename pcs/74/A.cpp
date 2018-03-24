0ac8896b9d7c7ad4d978f0733fe2fa5810d4dd28
Jacob Merizian <jmerizia@vt.edu>
2018-03-24 19:46:48 -0400

    contest 74 on dp

--- pcs/74/A.cpp
+++ pcs/74/A.cpp
@@ -4,8 +4,8 @@ using namespace std;
 typedef long long LL;
 
 LL a;
-vector<int> v;
-LL dp[(int)1e8];
+vector<LL> v;
+LL dp[100];
 
 int main()
 {
@@ -18,13 +18,7 @@ int main()
   LL nxt;
   while (ss >> nxt) v.push_back(nxt);
 
-  sort(v.begin(), v.end());
-
-  dp[0] = v[0];
-  for (int i = 1; i < v.size(); i++) dp[i] = dp[i-1]*v[i];
-  LL mn = a+1;
-  for (int i = 0; i < v.size(); i++) mn = min(mn, dp[i]+a/dp[i]); 
-  cout << mn;
 
   return 0;
 }
+
