796f8b72336fe55c72622adea54d3fde0f33eb9c
Jacob Merizian <jmerizia@vt.edu>
2018-11-05 15:47:17 -0500

    still not working

--- kattis/random/fire2/a.cpp
+++ kattis/random/fire2/a.cpp
@@ -6,24 +6,24 @@ typedef pair<int, int> pii;
 
 int T, w, h;
 vector<string> board;
-map<pii, int> mem;
+int closest_fire[1001][1001];
 vector<pii> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-int closest_fire(pii pos) {
-    if (mem.find(pos) != mem.end()) {
-        return mem[pos];
-    }
+int init_closest_fires() {
     queue<pii> Q;
     map<pii, int> depth;
-    Q.push(pos);
-    depth[pos] = 0;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (board[i][j] == '*') {
+                pii pos = {i, j};
+                Q.push(pos);
+                depth[pos] = 0;
+            }
+        }
+    }
     while (!Q.empty()) {
         pii u = Q.front(); Q.pop();
         int x, y; tie(x, y) = u;
-        if (board[x][y] == '*') {
-            mem[pos] = depth[u];
-            return depth[u];
-        }
         for (pii dir : dirs) {
             int _x, _y; tie(_x, _y) = dir;
             if (x+_x >= 0 && x+_x < h && y+_y >= 0 && y+_y < w) {
@@ -31,13 +31,13 @@ int closest_fire(pii pos) {
                     pii v = {x+_x,y+_y};
                     if (depth.find(v) == depth.end()) {
                         depth[v] = depth[u]+1;
+                        closest_fire[v.first][v.second] = depth[v];
                         Q.push(v);
                     }
                 }
             }
         }
     }
-    mem[pos] = INT_MAX;
     return INT_MAX;
 }
 
@@ -56,7 +56,7 @@ int can_make_it(pii start) {
             if (x+_x >= 0 && x+_x < h && y+_y >= 0 && y+_y < w) {
                 if (board[x+_x][y+_y] == '.') {
                     pii v = {x+_x,y+_y};
-                    if (closest_fire(v) >= next_depth) {
+                    if (closest_fire[v.first][v.second] > next_depth) {
                         if (depth.find(v) == depth.end()) {
                             depth[v] = next_depth;
                             Q.push(v);
@@ -83,12 +83,12 @@ int main()
     cin >> T;
     while (T--) {
         board.clear();
-        mem.clear();
         cin >> w >> h;
         for (int i = 0; i < h; i++) {
             string row; cin >> row;
             board.push_back(row);
         }
+        init_closest_fires();
         pii start = find_start();
         int ans = can_make_it(start);
         if (ans == -1) printf("IMPOSSIBLE\n");
