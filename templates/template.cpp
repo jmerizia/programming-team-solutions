54cacdcf735003c83180afae024c2314de9e8bb3
Jacob Merizian <jmerizia@vt.edu>
2020-01-26 01:44:44 -0500

    updated template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> pii;
 #define CONTAINS(set, x) (set.find(x) != set.end())
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
 
 void Solve()
 {
